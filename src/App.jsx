604844a1bf5c28672742842ff1cbb88fe924ea07
ayush <ayush.xyz1625@gmail.com>
2023-10-20 13:41:21 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+/* eslint-disable no-unused-vars */
 import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import Profile from './Pages/Profile'
+
+
 
 function App() {
 
   return (
-    <>
-      ello
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/sign-in' element={<SignIn/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+        <Route path='/profile' element={<Profile/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/Pages/SignUp.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div>
+      Sign Up
+    </div>
+  )
+}
+
+export default SignUp

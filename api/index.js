be2e815f13d3e2dd26d7ee6dcb666c8017d683d7
ayush <ayush.xyz1625@gmail.com>
2023-10-23 00:32:17 +0530

    Complete sign up page functionality

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 
+
+
 dotenv.config();
+
 
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("connected to database :)");
@@ -19,6 +21,7 @@ const app = express();
 app.use(express.json());
 
 
+
 app.listen(3000, ()=>{
     console.log("server running at local host 3000");
 })
@@ -27,6 +30,8 @@ app.listen(3000, ()=>{
 app.use('/api/user', userRouter);
 
 app.use('/api/auth', authRouter);
+
+
 
 
 
--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,19 +1,77 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
 import './SignUp.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const SignUp = () => {
+
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+
+      setLoading(true);
+
+      const res = await fetch("/api/auth/signup", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      console.log(data);
+
+      if(data.success === false){
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+
+      setLoading(false);
+      setError(null);
+
+      navigate('/sign-in')
+      
+    } catch (error) {
+      console.log("some error occured");
+      setLoading(false);
+      setError(error.message);
+      console.log(error);
+    }
+
+    
+  }
+
+
+
   return (
     <div className='signup-container'>
       <h1>SIGN UP</h1>
 
-      <form>
-        <input type="text" placeholder='username' className='username' id='username' />
-        <input type="email" placeholder='email' className='email' id='email' />
-        <input type="password" placeholder='password' className='password' id='password' />
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='username' className='username' id='username' onChange={handleChange} />
+        <input type="email" placeholder='email' className='email' id='email' onChange={handleChange} />
+        <input type="password" placeholder='password' className='password' id='password' onChange={handleChange}  />
 
-        <button className='manual-sign-up'>SIGN UP</button>
+        <button disabled={loading} className='manual-sign-up'> 
+          {loading? "LOADING..." : "SIGN UP"}
+        </button>
       </form>
 
       <div className='account-exist'>

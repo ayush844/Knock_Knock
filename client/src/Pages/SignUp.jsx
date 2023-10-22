18c9865ed66c85b2704e358e14a414cc38c3e3d0
ayush <ayush.xyz1625@gmail.com>
2023-10-22 18:16:39 +0530

    completed sign up page UI

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,10 +1,25 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import './SignUp.css'
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
   return (
-    <div>
-      Sign Up
+    <div className='signup-container'>
+      <h1>SIGN UP</h1>
+
+      <form>
+        <input type="text" placeholder='username' className='username' id='username' />
+        <input type="email" placeholder='email' className='email' id='email' />
+        <input type="password" placeholder='password' className='password' id='password' />
+
+        <button className='manual-sign-up'>SIGN UP</button>
+      </form>
+
+      <div className='account-exist'>
+        <p>Have an account ? <Link to={'/sign-in'}><span>SignIn</span></Link></p>
+        
+      </div>
     </div>
   )
 }

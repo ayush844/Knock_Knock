ce49d08f4ff936377bec0661d83d873053db6e0c
ayush <ayush.xyz1625@gmail.com>
2023-10-28 00:02:41 +0530

    Add redux persist

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -1,11 +1,26 @@
 /* eslint-disable no-unused-vars */
-import { configureStore} from '@reduxjs/toolkit'
+import { combineReducers, configureStore} from '@reduxjs/toolkit'
 
 import userReducers from './user/userSlice.js'
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
+
+const rootReducer = combineReducers({user: userReducers});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  verion: 1
+}
+
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    user: userReducers
-  },
+  reducer: persistedReducer,
+  
 
 })
+
+
+export const persistor = persistStore(store);

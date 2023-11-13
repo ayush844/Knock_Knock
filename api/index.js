bc3ee3562949866f5c6724c15eccbd5fc15340d1
ayush <ayush.xyz1625@gmail.com>
2023-11-13 15:15:32 +0530

    Created update user API route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 
 
@@ -20,7 +21,7 @@ const app = express();
 
 app.use(express.json());
 
-
+app.use(cookieParser());
 
 app.listen(3000, ()=>{
     console.log("server running at local host 3000");

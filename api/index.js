e5638f0c9399365544d9aded2287067ec587f909
ayush <ayush.xyz1625@gmail.com>
2023-11-23 23:26:34 +0530

    adding pre installation codes

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 
+import path from 'path';
+
 
 
 dotenv.config();
@@ -16,6 +18,8 @@ mongoose.connect(process.env.MONGO).then(()=>{
 }).catch((err) => {
     console.log(err);
 })
+
+const __dirname = path.resolve();
 
 
 const app = express();
@@ -36,7 +40,11 @@ app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 
 
